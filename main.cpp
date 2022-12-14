94adfcd123c11a8bf44e19cae35e24d8b16be056
filippoanselmi <116039968+filippoanselmi@users.noreply.github.com>
2022-12-14 17:12:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,25 +6,25 @@ using namespace std;
 int main(){
   char lista[10][20];
   char nome[20];
-  for (int x=0; x<10; x++) {
-    for (int y=0; y<20; y++) {
-      lista[x][y]=' ';
+  for (int x = 0; x < 10; x++) {
+    for (int y = 0; y < 20; y++) {
+      lista[x][y] = ' ';
     }
   }
-  for (int x=0; x<20; x++) {
-    nome[x]=' ';
+  for (int x = 0; x < 20; x++) {
+    nome[x] = ' ';
   }
-  for (int x=0; x<10; x++) {
+  for (int x = 0; x < 10; x++) {
     cin >> lista[x];
   }
-  for (int x=0; x<10; x++) {
-    cout << lista [x] << endl;
+  for (int x = 0; x < 10; x++) {
+    cout << lista[x] << endl;
   }
   cin >> nome;
-  int x=bodyguard(lista, nome);
-  if (x<10) {
+  int x = bodyguard(lista, nome);
+  if (x < 10) {
     cout << x;
-  } else if (x==10) {
+  } else if (x == 10) {
     cout << "non presente";
   }
   return 0;
