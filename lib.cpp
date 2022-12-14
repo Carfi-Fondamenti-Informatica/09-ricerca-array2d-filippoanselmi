8a462af2d863b8f160a22e9bfa103c15ef2ae3ad
filippoanselmi <116039968+filippoanselmi@users.noreply.github.com>
2022-12-14 17:15:16 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,11 @@
-int bodyguard (char lista[][20], char nome[20]) {
-  int i=0, j=0;
-  while ((j<19)and(i<10)) {
-    if (lista[i][j]!=nome[j]) {
+int bodyguard(char lista[][20], char nome[20]){
+  int i = 0;
+  int j = 0;
+  while ((j < 19) and (i < 10)) {
+    if (lista[i][j] != nome[j]) {
       i++;
-      j=0;
-    } else if (lista[i][j]==nome[j]) {
+      j = 0;
+    } else if (lista[i][j] == nome[j]) {
       j++;
     }
   } return i;
